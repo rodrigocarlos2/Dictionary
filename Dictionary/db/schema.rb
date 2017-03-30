a52ba9f127057a1a76b56704fbe30321bff088ad
Rodrigo <rodrigo19962010@live.com>
2017-03-30 09:04:35 -0300

    Add meaning to Project

--- Dictionary/db/schema.rb
+++ Dictionary/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330115816) do
+ActiveRecord::Schema.define(version: 20170330120106) do
+
+  create_table "meanings", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["word_id"], name: "index_meanings_on_word_id"
+  end
 
   create_table "words", force: :cascade do |t|
     t.text     "text"
